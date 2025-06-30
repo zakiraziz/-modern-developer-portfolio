47b79867ffa245818050095dccec33de297dc9e2
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-06-30 19:58:56 +0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,27 +3,117 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintPluginImport from 'eslint-plugin-import'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
+import eslintPluginPromise from 'eslint-plugin-promise'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', '*.config.js', '*.d.ts']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      import: eslintPluginImport,
+      unicorn: eslintPluginUnicorn,
+      promise: eslintPluginPromise,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      'plugin:import/recommended',
+      'plugin:unicorn/recommended',
+      'plugin:promise/recommended',
+      'plugin:prettier/recommended',
     ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx'],
+        },
+      },
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
+      // Base JavaScript rules
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
+      
+      // Import rules
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'import/no-default-export': 'off',
+      'import/no-unresolved': 'error',
+      
+      // React rules
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      
+      // Unicorn rules
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            camelCase: true,
+            pascalCase: true,
+          },
+        },
+      ],
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          replacements: {
+            props: false,
+            ref: false,
+          },
+        },
+      ],
+      
+      // Promise rules
+      'promise/always-return': 'error',
+      'promise/no-return-wrap': 'error',
+      
+      // Prettier integration
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.test.{js,jsx}'],
+    env: {
+      jest: true,
+    },
+    rules: {
+      'no-unused-expressions': 'off',
     },
   },
 ])
